b36ff42aa90b0057e03402eecef7a28f98299a19
Ikyu <muhiqballukman@gmail.com>
2018-03-24 16:21:01 +0700

    0.0.1
    (NEW)PUMA MEMBER:
    New puma member content

---
+++ src/components/Dashboard/Views/Puma.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <paper-table :title="listTable.title" :sub-title="listTable.subTitle" :data="listTable.data" :columns="listTable.columns">
+          </paper-table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PaperTable from 'components/UIComponents/PaperTable.vue'
+const tableColumns = ['id', 'Fullname', 'Position', 'Email']
+var adminMember = [{
+  id: '0012015000**',
+  fullname: 'Muhammad Iqbal Lukman',
+  position: 'Web Dev',
+  email: 'muhiqballukman@gmail.com'
+},
+{
+  id: '0012015000**',
+  fullname: 'Rakha Viantoni Prastya',
+  position: 'Web Dev',
+  email: 'rakhaviantoni'
+},
+{
+  id: '0012015000**',
+  fullname: 'Teuku Hilman Revanda',
+  position: 'Web Dev',
+  email: 'teukuatjeh'
+}
+]
+export default {
+  components: {
+    PaperTable
+  },
+  data () {
+    return {
+      listTable: {
+        title: 'Dashboard Member',
+        subTitle: 'Member that have access to this table',
+        columns: [...tableColumns],
+        data: [...adminMember]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,7 @@ import Icons from 'src/components/Dashboard/Views/Icons.vue'
 import Maps from 'src/components/Dashboard/Views/Maps.vue'
 import Typography from 'src/components/Dashboard/Views/Typography.vue'
 import TableList from 'src/components/Dashboard/Views/TableList.vue'
+import Puma from 'src/components/Dashboard/Views/Puma.vue'
 
 const routes = [
   {
@@ -57,6 +58,11 @@ const routes = [
         path: 'table-list',
         name: 'table-list',
         component: TableList
+      },
+      {
+        path: 'puma-member',
+        name: 'puma member',
+        component: Puma
       }
     ]
   },
